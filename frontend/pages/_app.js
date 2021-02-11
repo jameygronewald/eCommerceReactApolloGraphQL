d00f7b1ebc8d01916a5d9e4f37d64f7957f0ede8
Jamey Gronewald <jrgronewald@gmail.com>
2021-02-11 08:35:08 -0800

    added nprogress bar during route change/completion

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,12 @@
-import Page from "../components/Page";
+import NProgress from 'nprogress';
+import Page from '../components/Page';
+import Router from 'next/router';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
+import '../components/styles/nprogress.css'
 
 export default function MyApp({ Component, pageProps }) {
   return (
