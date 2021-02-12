542919c56e30fd48b3138a40e4ff4cdbbc98238d
Jamey Gronewald <jrgronewald@gmail.com>
2021-02-12 10:46:08 -0800

    added mutation to sell page to create a product; mutation successfully adding product data to backend and clearing form upon creation; also handling invalid input errors with DisplayError component

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,19 +1,63 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
 import FormStyles from './styles/Form';
+import DisplayError from './ErrorMessage';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    #variables to pass in w types
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, resetForm, clearForm } = useForm({
+    image: '',
     name: '',
     price: 0,
     description: '',
   });
 
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
+
   return (
-    <FormStyles>
-      <fieldset>
+    <FormStyles
+      onSubmit={async e => {
+        e.preventDefault();
+        await createProduct();
+        clearForm();
+      }}
+    >
+      <DisplayError error={error}/>
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor='image'>
           Image
           <input
+            required
             type='file'
             id='image'
             name='image'
@@ -39,6 +83,16 @@ export default function CreateProduct() {
             name='price'
             placeholder='Price'
             value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor='description'>
+          Description
+          <textarea
+            id='description'
+            name='description'
+            placeholder='Description'
+            value={inputs.description}
             onChange={handleChange}
           />
         </label>
