9a05f2a81cf9287da87d4f79243f0b290f8ab616
Jamey Gronewald <jrgronewald@gmail.com>
2021-02-28 13:14:29 -0800

    added custom mutation to add and item to cart or increment quantity by one if item already exists in user's cart

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 import CartStyles from './styles/CartStyles';
 import Supreme from './styles/Supreme';
+import CloseButton from './styles/CloseButton';
 import { useUser } from './User';
 import formatMoney from '../lib/formatMoney';
 import calcTotalPrice from '../lib/calcTotalPrice';
+import { useCart } from '../lib/CartState';
 
 const CartItemStyles = styled.li`
   padding: 1rem 0;
@@ -46,14 +48,15 @@ function CartItem({ cartItem }) {
 
 export default function Cart() {
   const me = useUser();
+  const { cartOpen, closeCart } = useCart();
+
   if (!me) return null;
 
-  console.log(me);
-
   return (
-    <CartStyles open={true}>
+    <CartStyles open={cartOpen}>
       <header>
         <Supreme>{me.name}'s Cart</Supreme>
+        <CloseButton onClick={closeCart}>&times;</CloseButton>
       </header>
       <ul>
         {me.cart.map(cartItem => (
