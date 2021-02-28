3a5bc2d1226bca7be332887c5cc57e369aaaa99a
Jamey Gronewald <jrgronewald@gmail.com>
2021-02-28 14:58:03 -0800

    added RemoveFromCart component button to each cart item and successfully deleting cart item on click of button

---
+++ frontend/components/RemoveFromCart.js
@@ -0,0 +1,26 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+import { CURRENT_USER_QUERY } from './User';
+
+const REMOVE_FROM_CART_MUTATION = gql`
+  mutation REMOVE_FROM_CART_MUTATION($id: ID!) {
+    deleteCartItem(id: $id) {
+      id
+    }
+  }
+`;
+
+export default function RemoveFromCart({ id }) {
+  const [removeFromCart, { loading }] = useMutation(REMOVE_FROM_CART_MUTATION, {
+    variables: {
+      id,
+    },
+    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+  });
+
+  return (
+    <button disabled={loading} type='button' onClick={removeFromCart}>
+      {loading ? 'Removing' : 'Remove'} from Cart -
+    </button>
+  );
+}
